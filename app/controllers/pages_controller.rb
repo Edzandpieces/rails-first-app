e3d7ca1e44123c46cb722d5ff39971d28f1fa833
Edmund Oh <eddieoh0@gmail.com>
2022-05-09 10:55:03 +0900

    cleaned up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,17 @@
 class PagesController < ApplicationController
-
   def home
-    @time = Time.today
+    @time = Time.now
   end
 
   def about
+    # automatically renders about.html.erb
   end
 
   def contact
-  else search = params[:member]
-    raise
-    @members = ['Ben', 'Rebecca', 'Katherine', 'Alex', 'Celine', 'Lucien']
-
-    if search.present?
-      @members = @members.select{ |member| member.start_with?(params[:member]) }
-    # remember to Keep the search context using params in your input.
+    @members = [ 'Ben', 'Rebecca', 'Katherine', 'Alex', 'Celine', 'Lucien' ]
+    if (params[:member])
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+      # remember to Keep the search context using params in your input.
     end
   end
 end
